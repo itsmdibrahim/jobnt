318ca0911ad7574c0729fb4a76c921fa01ad6fa3
itsmdibrahim <95708746+itsmdibrahim@users.noreply.github.com>
2024-09-07 19:12:51 +0600

    Add files via upload

---
+++ backlinks.js
@@ -0,0 +1,8 @@
+var tenolentBacklink = Object.assign(document.createElement('a'), {
+    href: 'https://jobnt-tenolent.blogspot.com',
+    target: '_blank',
+    rel: 'noopener noreferrer',
+    textContent: 'Jobnt Blogger Template by Tenolent',
+    style: 'display:none'
+});
+document.getElementById('single-content').appendChild(tenolentBacklink);
